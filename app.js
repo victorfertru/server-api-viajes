f415ada0dd3fe75b3965d0c008edc469bbd7f8b1
Vic <victor.fertru@gmail.com>
2021-06-24 13:09:34 +0200

    Delete public folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
+
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
@@ -18,7 +18,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
