ca7d0c97e658be84ae34ed600b8e129e96625481
Vic <victor.fertru@gmail.com>
2021-06-30 23:53:10 +0200

    search + pagination

--- repositories/viajeRepository.js
+++ repositories/viajeRepository.js
@@ -46,22 +46,45 @@ exports.findAllTravels = async ({ pageSize, page, sort }) => {
 };
 
 exports.searchTravel = async (filtro) => {
-  const { tipoDeViajeId, nombre, destino } = filtro;
-
   const where = {};
+  let orderBy = "ASC";
+  let sortBy = "createdAt";
+  const order = [];
+  let offset = 0;
+  let limit = 5;
 
-  if (tipoDeViajeId) {
-    where.tipoDeViajeId = tipoDeViajeId;
-  }
-  if (nombre) {
-    where.nombre = { [Op.like]: `%${nombre}%` };
-  }
-  if (destino) {
-    where.destino = { [Op.like]: `%${destino}%` };
+  if (filtro) {
+    let { tipoDeViajeId, nombre, destino, pageSize, page, sort } = filtro;
+
+    offset = +pageSize * (page - 1);
+    limit = +pageSize;
+
+    if (sort) {
+      sortBy = sort;
+      if (sort.includes("-")) {
+        orderBy = "DESC";
+        sortBy = sort.replace("-", "");
+      }
+    }
+
+    if (tipoDeViajeId) {
+      where.tipoDeViajeId = tipoDeViajeId;
+    }
+    if (nombre) {
+      where.nombre = { [Op.like]: `%${nombre}%` };
+    }
+    if (destino) {
+      where.destino = { [Op.like]: `%${destino}%` };
+    }
   }
 
-  return await Viaje.findAll({
+  order.push([sortBy, orderBy]);
+
+  return await Viaje.findAndCountAll({
     where,
+    limit,
+    offset,
+    order,
     include: {
       model: TipoDeViaje,
       attributes: [["valor", "tipoDeViajeDesc"]],
