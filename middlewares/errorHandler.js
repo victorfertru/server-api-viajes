79d8be5ebd026f93c7db6d5925860f176740210f
Vic <victor.fertru@gmail.com>
2021-07-01 10:49:45 +0200

    Updated errorHandler middleware to handle jwt errors and return them as status 401

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,15 @@
 const errorHandler = (error, req, res, next) => {
   const status = error.statusCode ?? 500;
+
+  if (
+    error.name === "JsonWebTokenError" ||
+    error.name === "TokenExpiredError" ||
+    error.name === "NotBeforeError"
+  ) {
+    res.status(401).send({ message: error.message });
+    return;
+  }
+
   res.status(status).json({ message: error.message });
 };
 
